a795c0a84465f1f1bad79488be15374532797601
rasna shakya <rasnashakya@gmail.com>
2017-01-27 12:46:35 +0545

    previous would skip line if it exactly matches curent header row but problemaiseifwe havetochange header row, add columns orupdate. so this issolution of it

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,11 @@
 puts "EventManager Initialized!"
 
 lines = File.readlines "event_attendees.csv"
+row_index=0
 lines.each do |line|
- columns = line.split(",")
+	row_index +=1
+	next if row_index==1
+	columns = line.split(",")
   name = columns[2]
   puts name
 end
